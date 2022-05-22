d7db2ea1795087c1844b2cd4ed917629afab29c2
adamskirees <adamskirees@yahoo.com>
2022-05-22 12:48:06 +1000

    Nav bar style working now.

--- projects/landing-page/app.js
+++ projects/landing-page/app.js
@@ -18,15 +18,13 @@
  * ---Then put this array into the nav with an inner HTML (or similar)
 */
 // 
-const NavBar = document.getElementById("navbar__list");
-let sectionList = document.querySelectorAll("section");
+const NavBar = document.getElementById('navbar__list');
+let sectionList = document.querySelectorAll('section');
 
 //GET A NODE LIST OF THE SECTIONS
 function getSectionId(section) {
     return section.id;
 }
-
-   
     //add LI items to the navbar__list parent ADD LI ITEMS TO THE NAVBAR__LIST PARENT
 
 //SET UP A LOOP, THEN BUILD THE NEW LI FOR EACH
@@ -35,28 +33,33 @@ for (section of sectionList) {
     //GET THE ACTUAL TITLE OF THE SECTION
     let title = document.querySelector("h2").innerText;
     //CREATE THE LI ELEMENT FOR NAV FOR RE-USE, INCLUDING THE CLASS LISTS
-    const listElement = document.createElement("li");
+    const listElement = document.createElement('li');
     //CREATE THE ANCHOR AND THEN APPEND IN A LINK
     const HREF = document.createElement('a');
     //const sectionId = getSectionId(sectionList);
     HREF.setAttribute("href", "#" + section.id);
     listElement.appendChild(HREF);
     //GET THE RIGHT SECTION INTO EACH NAV LINK
+    // Simple blog post assistend to assing the classes via javascript, thanks to https://bobbyhadz.com/blog/javascript-create-element-with-class
     HREF.innerText = section.id;
     // ADD SOME APPROPRIATE CLASSES for sytling, see reference below
-    listElement.classList.add('menu__link');
-    //Append the above to the Navigation list
+    HREF.classList.add('menu__link');
+   //Append the above to the Navigation list
     NavBar.appendChild(listElement);
+
+        //look for and alter the active class in the navigation
+        if (section.classList.contains('your-active-class')) {
+            HREF.classList.add('your-active-class');
+        }
+
 }
-
 // ADD CLASS TO MENU 
-// Simple blog post assistend to assing the classes via javascript, thanks to https://bobbyhadz.com/blog/javascript-create-element-with-class
 
 
 
 
- // CREATE THE LI ELEMENT
+ // CREATE THE LI ELEMENT - *** done *** May need to refactor the loop.
  // CREATE ANY OTHER HTML ELEMENTS
- // APPEND IN ANY CLASS NAMES TO THE LI
+ // APPEND IN ANY CLASS NAMES TO THE LI  *** done*** 
  // APPEND IN THE INNER HTML TO THE LI (AND HREF REQUIRED)
  //FINALLY APPEND THE NEWLY CREATED LI ELEMENT + INFO TO THE NAVBAR_LIST
