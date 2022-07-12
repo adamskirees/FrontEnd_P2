dbce1d7923fbff875fb467532e9e1c60dd4e6697
adamskirees <adamskirees@yahoo.com>
2022-07-12 20:47:35 +1000

    code and comments tidy up

--- projects/landing-page/README.md
+++ projects/landing-page/README.md
@@ -1,10 +1,12 @@
 # Landing Page Project - Udacity front end nanodegree
+
+## Submitted by Adam Rees
 Creating a landing page with vanilla javascript that manipulates the DOM. Creates a dynamic landing page where navigation updates with additonal sections. 
 ## Description
-    * What it does? 
-    * What technolgis
-    * Challanges
-
+    * The javascript code manipulates the dom turning a static web page into dynamic; navigation updates with content section updates/ additions. click and mouse events add styling and smooth scrolling features to page. 
+    * HTML & CSS files. Majority of the coding changes were made in javascript file. 
+    * Showing and hiding the navigation bar with settimeout() javascript funciton was attempted, however showing some bugs when scrolling. 
 
 ## Credits
-    * 
+    * https://bobbyhadz.com/blog/javascript-create-element-with-class
+    * Stack Overflow - https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link
--- projects/landing-page/app.js
+++ projects/landing-page/app.js
@@ -11,29 +11,27 @@
  */
 /**
  * Define Global Variables
- * ---We need to track the active section. 
- * ---WE need to track the active nav section as well
- * ---We need to track where we are on the screen (which section is showing)
- * ---We need to make an array of the sections
- * ---Then put this array into the nav with an inner HTML (or similar)
+ *We need to track the active section first, and then subsequently the active nave. 
+ * Will also need to make an array of the sections, converting these into Nav ul/li 
 */
 
-// MY CODE BEGIN **************************************************************************************** 
+// MY CODE BEGIN, VARIABLES 
 const NavBar = document.getElementById('navbar__list');
 let sectionList = document.querySelectorAll('section');
 
 
-//GET A NODE LIST OF THE SECTIONS***********************************************************************
+//GET A NODE LIST OF THE SECTIONS
 function getSectionId(section) {
     return section.id;
 }
 
-//add LI items to the navbar__list parent ADD LI ITEMS TO THE NAVBAR__LIST PARENT
-// IDENTIFY THE ACTIVE SECTION OF THE PAGE, SO THE ACTIVE SECTION IS UPDATED AND UPDATES THE LI AS WELL
-// HAVE REFERED TO UDACITY MENTOR ANSWER FOR THIS...https://knowledge.udacity.com/questions/85408*****
-// REMAINED WITH THIS METHOD EVEN WITH SMALL SCREEN ISSUES, INSTEAD OF USING INTERSECTION OBSERVER API
+/**add LI items to the navbar__list parent ADD LI ITEMS TO THE NAVBAR__LIST PARENT
+// IDENTIFY THE ACTIVE SECTION OF THE PAGE, SO THE ACTIVE SECTION IS UPDATED 
+*  AND UPDATES THE LI AS WELL. HAVE REFERED TO UDACITY MENTOR ANSWER FOR THIS
+// ...https://knowledge.udacity.com/questions/85408*****
+*/
     function isElementInViewport(el) {
-        var rect = el.getBoundingClientRect();
+        let rect = el.getBoundingClientRect();
         return (
           rect.top >= 0 &&
           rect.left >= 0 &&
@@ -42,13 +40,12 @@ function getSectionId(section) {
         );
       }
                             
-//SET UP A LOOP, THEN BUILD THE NEW LI FOR EACH SECTION WE HAVE***************************************
+//SET UP A LOOP, THEN BUILD THE NEW LI FOR EACH SECTION
 for (section of sectionList) {
     //CREATE THE LI ELEMENT FOR NAV FOR RE-USE, INCLUDING THE CLASS LISTS
     const listElement = document.createElement('li');
     //CREATE THE ANCHOR AND THEN APPEND IN A LINK
     const HREF = document.createElement('a');
-    //const sectionId = getSectionId(sectionList);
     HREF.setAttribute("href", "#" + section.id);
     listElement.appendChild(HREF);
     //GET THE RIGHT SECTION INTO EACH NAV LINK
@@ -65,8 +62,9 @@ for (section of sectionList) {
         }
 }
 
-//CALL THE FUNCTION ABOVE UPON SCROLL, USING THE sectionList **************************************
-//THIS POST WAS READ SEVERAL TIMES TO ASSIST https://knowledge.udacity.com/questions/66312#66326
+//CALL THE FUNCTION ABOVE UPON SCROLL, USING THE sectionList 
+//THIS POST WAS READ SEVERAL TIMES TO 
+//ASSIST https://knowledge.udacity.com/questions/66312#66326
 window.document.addEventListener(
     "scroll",
     function (event) {
@@ -82,9 +80,8 @@ window.document.addEventListener(
 );
 
 //AS PER ABOVE WITH ACTIVE SECTOIN, Get a Nav list and then UPDATE THE ACTIVE NAV UPON SCROLL
-// GET A LIST OF NAV ITEMS*********************************************************************
+// Get a nav list of items
 let navsList = document.querySelectorAll("ul .menu__link");
-//console.log(navsList);
 
 window.document.addEventListener("scroll", () => {
         let sectionId = "";
@@ -96,9 +93,8 @@ window.document.addEventListener("scroll", () => {
             }    
     });
            
-//loop to update the Nav highlighting ********************************************************
-//console.log(sectionId);
-navsList.forEach((li) => {
+//loop to update the Nav highlighting 
+    navsList.forEach((li) => {
     if (li.classList.contains(sectionId)) {
         li.classList.add("your-active-class");
     } else {
@@ -108,35 +104,32 @@ navsList.forEach((li) => {
 });
 
 
-// IMPLIMENT A SMOOTH SCROLL FROM THE CLICK OF THE MENU LI*****************************************
-//CLICK EVENT
+// IMPLIMENT A SMOOTH SCROLL FROM THE CLICK OF THE MENU LI (click event)
 // StackOverflow post was most helpful...https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
         });
     });
 });
 
 
 //USE SETTIMEOUT TO HIDE THE NAVIGATION WHEN NOT SCROLLING
-//function hideNav() => document.querySelector('nav').hideNav;
 const task = function() {   
     let navchk = document.querySelector('nav');
     if(window.getComputedStyle(navchk).display == "block") {
         document.querySelector('nav').style.display = "none";
         //window.clearTimeout();
-    }
+        }
          else {
            document.querySelector('nav').style.display = "block";
         }
     };  
     window.document.addEventListener("scroll", () => {
-        setTimeout(task, 1000)
-    });
+        setTimeout(task, 5000)
+        });
 
      
     
